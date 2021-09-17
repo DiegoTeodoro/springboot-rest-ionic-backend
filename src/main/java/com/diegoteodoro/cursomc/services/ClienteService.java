909661562627ce0401b6def219cb3bbfa9a426ec
Diego Teodoro <diegoteodoro_alves@yahoo.com.br>
2021-09-17 13:47:18 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/diegoteodoro/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.diegoteodoro.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegoteodoro.cursomc.domain.Cliente;
+import com.diegoteodoro.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) { 
+		 Optional<Cliente> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName(), null)); 
+		} 
+
+}
