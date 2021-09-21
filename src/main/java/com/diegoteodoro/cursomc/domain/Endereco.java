56c3acf6f47810c8c5a18019016fce6fe178c6f3
Diego Teodoro <diegoteodoro_alves@yahoo.com.br>
2021-09-21 10:34:46 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/diegoteodoro/cursomc/domain/Endereco.java
+++ src/main/java/com/diegoteodoro/cursomc/domain/Endereco.java
@@ -9,31 +9,30 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY) 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String logradouro;
 	private String numero;
 	private String complemento;
 	private String bairro;
 	private String cep;
-	
-	@JsonBackReference
+
+	@JsonIgnore
 	@ManyToOne
-	@JoinColumn(name="cliente_id")
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
-	
+
 	@ManyToOne
-	@JoinColumn(name="cidade_id")
+	@JoinColumn(name = "cidade_id")
 	private Cidade cidade;
 
-	
 	public Endereco() {
 		super();
 	}
@@ -106,7 +105,7 @@ public class Endereco implements Serializable {
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-	
+
 	public Cidade getCidade() {
 		return cidade;
 	}
@@ -139,7 +138,5 @@ public class Endereco implements Serializable {
 			return false;
 		return true;
 	}
-
-	
 
 }
